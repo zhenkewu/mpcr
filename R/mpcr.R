796d1d027eed128d6dd163060c2051b5fcc24158
Zhenke Wu <zhenkewu@gmail.com>
2014-12-10 16:17:09 -0500

    add testthat both for testing and illustration

--- R/mpcr.R
+++ R/mpcr.R
@@ -116,7 +116,9 @@
                       X_nm_all = c("race","ageatint","hcc","livesalone","education","s10","sf36mcs","sf36pcs","gender","h1"),
                       X_nm_binary = c("livesalone","education","gender"),
                       X_nm_cat    = c("race","s10","h1"),
-                      X_nm_cont   = c("ageatint","hcc","sf36mcs","sf36pcs")){
+                      X_nm_cont   = c("ageatint","hcc","sf36mcs","sf36pcs"),
+                      BAYES_STATUS = FALSE,
+                      digit.round = 2){
 
 
         cat("=====primary outcome:",outcome,"=====","\n")
